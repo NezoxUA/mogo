9f3c4e8da43a27c2aa1be54a12cdcd9b93b8d05d
Nezox <olegpavlivwebdev1@gmail.com>
2021-01-02 10:09:52 +0100

    add functional and adaptive design

---
+++ js/app.js
@@ -0,0 +1,76 @@
+
+
+$(function () {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+
+
+    // fixed header 
+    checkScroll(scrollOffset);
+    $(window).on("scroll", function () {
+
+
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+
+
+        if (scrollOffset >= introH) {
+            header.addClass("header--fixed");
+        } else {
+            header.removeClass("header--fixed");
+        }
+    }
+
+    // smooth scroll
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data("scroll"),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active")
+        $this.addClass("active");
+
+        $("html , body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+
+
+    // menu nav toogle
+    $("#nav__toggle").on("click", function (event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("nav").toggleClass("active");
+    });
+
+
+    // Collapse
+
+    $("[data-collapse]").on("click", function (event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data("collapse");
+
+        $this.toggleClass("active");
+
+    });
+
+
+    // slider
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
